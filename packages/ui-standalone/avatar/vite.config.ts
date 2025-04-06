e6c4903a9e6279fe3acbf7038b141a61ed6f713a
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-06 16:16:26 +0700

    refactor(vite): remove vite plugins dev dependencies

--- packages/ui-standalone/avatar/vite.config.ts
+++ packages/ui-standalone/avatar/vite.config.ts
@@ -1,29 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+import { viteConfig } from "@react-beauty/vite";
+
 import packageManifest from "./package.json";
 
-export default defineConfig({
-  plugins: [react(), dts()],
-  build: {
-    outDir: "dist",
-    lib: {
-      entry: "./src/index.ts",
-      name: "index",
-      formats: ["es"],
-      fileName: "index",
-    },
-    rollupOptions: {
-      external(id, _, isResolved) {
-        if (isResolved) {
-          return false;
-        }
-
-        const peerDependencies = Object.keys(packageManifest.peerDependencies);
-        return peerDependencies.some((packageName) => {
-          return id === packageName || id.startsWith(`${packageName}/`);
-        });
-      },
-    },
-  },
-});
+export default defineConfig(viteConfig(packageManifest));
