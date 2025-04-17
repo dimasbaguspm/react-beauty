58b70018c031eac415d4f7a6eb045201d84c4a62
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-17 15:49:05 +0700

    feat(theme-manager): ship theme-manager

--- packages/interfaces/ui-avatar/src/avatar.stories.tsx
+++ packages/interfaces/ui-avatar/src/avatar.stories.tsx
@@ -1,17 +1,17 @@
-import { AvatarImage } from "./avatar";
+import { AvatarImage, AvatarInitial } from "./avatar";
 
 import type { Meta, StoryObj } from "@react-beauty/storybook";
 
 const meta = {
   title: "UI/Avatar",
-  component: AvatarImage,
+  component: AvatarInitial,
 } satisfies Meta<typeof AvatarImage>;
 
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    src: "https://picsum.photos/200/300",
+    children: "Avatar",
   },
 };
 export default meta;
--- packages/interfaces/ui-avatar/src/avatar.tsx
+++ packages/interfaces/ui-avatar/src/avatar.tsx
@@ -1,13 +1,11 @@
 import { FC, ReactNode } from "react";
 
-import styles from "./avatar.module.css";
-
 export interface AvatarImageProps {
   src: string;
 }
 
 export const AvatarImage: FC<AvatarImageProps> = () => {
-  return <div className={styles.rbAvatar}>Avatar Image</div>;
+  return <div>Avatar Image</div>;
 };
 
 export interface AvatarInitialProps {
@@ -15,5 +13,5 @@ export interface AvatarInitialProps {
 }
 
 export const AvatarInitial: FC<AvatarInitialProps> = () => {
-  return <div>Avatar Initial</div>;
+  return <div>bar</div>;
 };
