fc0582fa9cb98caf471bb2a7a5d939ea15884bbf
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-17 19:06:22 +0700

    refactor(internal): relocate css-token generator

---
+++ internal/css-token/src/config.js
@@ -0,0 +1,66 @@
+import StyleDictionary from "style-dictionary";
+import { formats, transformGroups } from "style-dictionary/enums";
+
+const getBuildpath = (platform, isDev) => {
+  if (Boolean(isDev)) {
+    return `../../packages/interfaces/ui-core/src/${platform}/`;
+  }
+  return `../../packages/interfaces/ui-core/dist/${platform}/`;
+};
+
+const themes = ["light", "dark"];
+
+const generator = async (isDev) => {
+  const sd = new StyleDictionary({
+    log: {
+      verbosity: "silent",
+      warnings: "warn",
+    },
+    source: themes.map((theme) => `../../packages/**/tokens/${theme}.json`),
+    platforms: {
+      light: {
+        transformGroup: transformGroups.css,
+        buildPath: getBuildpath("light", isDev),
+        options: {
+          formatting: {
+            fileHeaderTimestamp: true,
+          },
+        },
+        files: [
+          {
+            destination: "index.css",
+            format: formats.cssVariables,
+          },
+          {
+            destination: "font.css",
+            format: formats.cssFonts,
+          },
+        ],
+      },
+      dark: {
+        transformGroup: transformGroups.css,
+        buildPath: getBuildpath("dark", isDev),
+        options: {
+          formatting: {
+            fileHeaderTimestamp: true,
+          },
+        },
+        files: [
+          {
+            destination: "index.css",
+            format: formats.cssVariables,
+          },
+          {
+            destination: "font.css",
+            format: formats.cssFonts,
+          },
+        ],
+      },
+    },
+  });
+
+  await sd.buildAllPlatforms();
+};
+
+await Promise.all([generator(1), generator(0)]);
+console.log("done");
