de55b04d6066637f8b0efde46517c95eb2b0d823
Dimas Bagus P <79041448+dimasbaguspm@users.noreply.github.com>
2025-04-22 14:49:41 +0700

    feat(demo): add routing example with wouter (#81)

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./app";
+import { AppRouter } from "./core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 );
