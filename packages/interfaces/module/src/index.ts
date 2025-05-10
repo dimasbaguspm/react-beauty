ea5ec87594fa78448ce36f9426646e30181ff615
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-05-10 10:56:10 +0700

    refactor(ui): remove ui-core export from index.ts

--- packages/interfaces/module/src/index.ts
+++ packages/interfaces/module/src/index.ts
@@ -2,7 +2,6 @@
 
 export * from '@react-beauty/ui-avatar';
 export * from '@react-beauty/ui-icon';
-export * from '@react-beauty/ui-core';
 export * from '@react-beauty/ui-alert';
 export * from '@react-beauty/ui-button';
 export * from '@react-beauty/ui-tag';
